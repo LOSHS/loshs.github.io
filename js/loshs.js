2cda32700a2cf3564072efd3756868ffc0bbd045
hector <hector@loshs.com>
2016-07-24 20:07:47 -0500

    Fix: Localizedspanish text

--- js/loshs.js
+++ js/loshs.js
@@ -1,3 +1,4 @@
+var isMx = false;
 $(document).ready(function() {
 
 	$('.toggle').on('click', function() {
@@ -8,8 +9,38 @@ $(document).ready(function() {
 			$(hiddenDivId).toggle();
 		}
 		
-		$(this).text($(this).text() == 'less...' ? 'Learn more...' : 'less...');		
+		if(isMx) {
+			$(this).text($(this).text() == 'menos...' ? 'Saber más...' : 'menos...');
+		}
+		else {
+			$(this).text($(this).text() == 'less...' ? 'Learn more...' : 'less...');
+		}
 		
 		return false;
 	});	
+	
+	lookupCountryWithFreegeoip();		
 });
+
+// supports CORS and JSONP
+function lookupCountryWithFreegeoip() {
+    
+        var dfr = $.ajax({
+            url: 'http://freegeoip.net/json/',
+            dataType: $.support.cors ? 'json' : 'jsonp'
+        }).done(function(results){
+        		
+   			isMx = (results.country_code == 'MX');   
+   			localizeText(); 		
+        });
+}
+
+function localizeText() {
+	debugger;
+	if(isMx) {
+		
+		$.each($('.translatable'), function(index, element){
+			$(element).text($(element).data('spanishtext'));
+		});
+	}
+}
