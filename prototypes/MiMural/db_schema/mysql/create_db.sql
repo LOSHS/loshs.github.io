2bdc52014985e8a189e0098028066e0ad48ad7fb
HugoLabra <loshsconsulting@gmail.com>
2016-09-04 18:35:06 -0500

    Fix: Index for user_code

--- prototypes/MiMural/db_schema/mysql/create_db.sql
+++ prototypes/MiMural/db_schema/mysql/create_db.sql
@@ -42,7 +42,8 @@ CREATE TABLE `users` (
   `user_mail` varchar(45) DEFAULT NULL,
   `reputation` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
-  UNIQUE KEY `users_id_UNIQUE` (`user_id`)
+  UNIQUE KEY `users_id_UNIQUE` (`user_id`),
+  UNIQUE KEY `user_code_UNIQUE` (`user_code`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -52,7 +53,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Labra','Aguilar','Hugo','Daniel','LAAH000000XXX','$2a$10$8WJvJSaPEkLIN46f2nOgaeH3u/QfqCFpgg0wGuHGKkfENQqpg.0PK',NULL,'11','Director','Directivo',1,'2016-08-30','2020-01-01','hugo@loshs.com','5'),(2,'Herrera','Perez','Hector',NULL,'HEPH000000XXX','$2a$10$byrbl/IC/btdQzox49UPZOkN2UkhAyBg0NKYNvG8NyHGyr7zFv892',NULL,'12','Director','Directivo',1,'2016-08-30','2020-01-01','hector@loshs.com','5'),(3,'Perez','Ramirez','Hector','Giovanni','PERH000000XXX','$2a$10$ldUg6OCrrcd3wlI80w6YJ.sg7PLFdu.zOA4QLmmEefnFmWoI05FS2',NULL,'11','Subirector','Directivo',1,'2016-08-30','2020-01-01','gio@loshs.com','5'),(4,'Kurata','Hernandez','Mari','Kimi','KUHM000000XXX','$2a$10$Bixa/fJFZ5vzRPuhHcoqAuq6E/DKL.numA.cVbBiQ40uB9cY2JVjy',NULL,'12','Subirector','Directivo',1,'2016-08-30','2020-01-01','kimi@loshs.com','5');
+INSERT INTO `users` VALUES (1,'Labra','Aguilar','Hugo','Daniel','LAAH000000XXX','$2a$10$8WJvJSaPEkLIN46f2nOgaeH3u/QfqCFpgg0wGuHGKkfENQqpg.0PK',NULL,'11','Director','Directivo',1,'2016-08-30','2020-01-01','hugo@loshs.com','5'),(2,'Herrera','Perez','Hector',NULL,'HEPH000000XXX','$2a$10$byrbl/IC/btdQzox49UPZOkN2UkhAyBg0NKYNvG8NyHGyr7zFv892',NULL,'11','Superadmin','Superadmin',1,'2016-08-30','2020-01-01','hector@loshs.com','5'),(3,'Perez','Ramirez','Hector','Giovanni','PERH000000XXX','$2a$10$ldUg6OCrrcd3wlI80w6YJ.sg7PLFdu.zOA4QLmmEefnFmWoI05FS2',NULL,'12','Director','Directivo',1,'2016-08-30','2020-01-01','gio@loshs.com','5'),(4,'Kurata','Hernandez','Mari','Kimi','KUHM000000XXX','$2a$10$Bixa/fJFZ5vzRPuhHcoqAuq6E/DKL.numA.cVbBiQ40uB9cY2JVjy',NULL,'12','Subirector','Directivo',1,'2016-08-30','2020-01-01','kimi@loshs.com','5');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -65,4 +66,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-02  1:12:27
+-- Dump completed on 2016-09-04 18:34:28
